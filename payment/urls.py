052eef7ab12360183c556808a239c52b1eb94520
ndayanicet12 <ndayanicet12@gmail.com>
2017-07-18 05:16:55 -0400

    Updting some files

--- payment/urls.py
+++ payment/urls.py
@@ -5,4 +5,5 @@ from . import views
 app_name = 'payment'
 urlpatterns = [        
 	url(r'^$', views.CreatePayment.as_view(), name='createpayment'), 
+	url(r'^index/$', views.ListPayment.as_view(), name='index'), 
 ]
